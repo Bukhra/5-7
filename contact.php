d19ef8fbb5af421e7d51e45a5fc71fa708b03d21
Simon Bao <bukhrashvili.gega@gmail.com>
2024-04-09 21:18:28 +0400

    gavadinamiuret

---
+++ contact.php
@@ -0,0 +1,35 @@
+<?php
+$cards = [
+
+    [
+        'img' => './assets/lingerie-underwear.svg',
+        'name' => 'Lingerie',
+        'btnname' => 'View More',
+    
+    ],
+
+    [
+        'img' => './assets/dress.svg',
+        'name' => 'Spring',
+        'btnname' => 'View More',
+    ],
+
+    [
+        'img' => './assets/chakra.svg',
+        'name' => 'Yoga',
+        'btnname' => 'View More',
+    ],
+
+    [
+        'img' => './assets/lipstick.svg',
+        'name' => 'Glow',
+        'btnname' => 'View More',
+    ],
+
+    [
+        'img' => './assets/dress-wedding-dress.svg',
+        'name' => 'Ensembles',
+        'btnname' => 'View More',
+    ],
+
+];
--- index.php
+++ index.php
@@ -1,45 +1,77 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+    <?php require 'contact.php' ?>
 </head>
 <body>
-  <div>
-    <form action="" method="POST">
-      <input type="number" name="number1">
-      <select name="operand">
-        <option value="+">+</option>
-      </select>
-      <input type="number" name="number2">
-      <button>Click me</button>
-    </form>
-  </div>
-
-  <h1>
-    <?php
-          if (isset($_POST['operand']) && isset ($_POST['number1']) && isset ($_POST['number2'])) {
-              $num1 = $_POST['number1'];
-              $num2 = $_POST['number2'];
-              $operand = $_POST['operand'];
-
-              switch ($operand) {
-                case '+':
-                  echo $num1 + $num2;
-                  break;
-
-                 default :
-                 echo "error"; 
-              }
-          }
-          
-      
-
-
-
-?>
-  </h1>
-  
+    
 </body>
 </html>
+
+
+
+<div class="topics-container">
+          <h2>Topics</h2>
+          <div class="topic-cards">
+            
+            <?php
+
+          foreach ($cards as $card) {
+            echo ' <div class="topic-card">
+            <div class="topic-img">
+              <img src=" ' . ($card['img']) . ' " alt="1" />
+            </div>
+            <h3> ' . ($card['name']) . '</h3>
+            <button>View More</button>
+          </div>  ' ;
+          } 
+
+            ?>
+            <!-- <div class="topic-card">
+              <div class="topic-img">
+                <img src="./assets/lingerie-underwear.svg" alt="1" />
+              </div>
+              <h3>Lingerie</h3>
+              <button>View More</button>
+            </div>
+            <div class="topic-card">
+              <div class="topic-img">
+                <img src="./assets/dress.svg" alt="2" />
+              </div>
+              <h3>Spring</h3>
+              <button>View More</button>
+            </div>
+            <div class="topic-card">
+              <div class="topic-img">
+                <img src="./assets/chakra.svg" alt="3" />
+              </div>
+              <h3>Yoga</h3>
+              <button>View More</button>
+            </div>
+            <div class="topic-card">
+              <div class="topic-img">
+                <img src="./assets/lipstick.svg" alt="4" />
+              </div>
+              <h3>Glow</h3>
+              <button>View More</button>
+            </div>
+            <div class="topic-card">
+              <div class="topic-img">
+                <img src="./assets/dress-wedding-dress.svg" alt="5" />
+              </div>
+              <h3>Ensembles</h3>
+              <button>View More</button>
+            </div>
+            <div class="topic-card">
+              <div class="topic-img">
+                <img src="./assets/mask-theater.svg" alt="6" />
+              </div>
+              <h3>Skin Care</h3>
+              <button>View More</button>
+            </div> -->
+          </div>
+        </div>
